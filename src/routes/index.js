d731e71b56be9d080faed72efa4c50489c311fc0
Muhammad Kholil Aziz Ashari <muhammadkholilazizashari@gmail.com>
2025-11-04 16:18:32 +0700

    Add Login, SidebarMenu & Dashboard

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,14 @@
 // import vue router
 import { createRouter, createWebHistory } from 'vue-router'
 
+// import js cookie
+import Cookies from 'js-cookie'
+
+// Utility to get the token
+const getToken = () => {
+    return Cookies.get('token')
+}
+
 // define a routes
 const routes = [
     {
@@ -17,13 +25,41 @@ const routes = [
         path: '/login',
         name: 'login',
         component: () => import('../views/auth/login.vue')
-    }
+    },
+    {
+        path: '/admin/dashboard',
+        name: 'dashboard',
+        component: () => import('../views/admin/dashboard/index.vue'),
+        meta: { requiresAuth: true }
+    },
 ]
 
 // create router
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+// Global navigation guard
+router.beforeEach((to, from, next) => {
+    // Ambil token otentikasi pengguna
+    const token = getToken()
+
+    // Jika rute tujuan membutuhkan otentikasi dan pengguna tidak memiliki token
+    if (to.matched.some(record => record.meta.requiresAuth) && !token) {
+        // Alihkan pengguna ke halaman login
+        next({ name: 'login' })
+    }
+
+    // Jika rute tujuan adalah halaman login atau register dan pengguna sudah login
+    else if ((to.name === 'login' || to.name === 'register') && token) {
+        // Alihkan pengguna ke halaman dashboard
+        next({ name: 'dashboard' })
+    }
+
+    else {
+        next()
+    }
 })
 
 export default router
